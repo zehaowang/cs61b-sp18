0540c4c3d28ced4f2daf51f42b94b9b024144b8b
zehaowang <zw2323@columbia.edu>
2019-07-08 22:01:50 -0400

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,13 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int i = 0;
+        while (i < 10) {
+
+            x = x + i;
+            System.out.print(x + " ");
+            i += 1;
+        }
+        System.out.println("");
+    }
+}
