7137ff25cc2e8f6c4cc6dc932ebad74bbf2a1137
zehaowang <zw2323@columbia.edu>
2019-08-11 19:33:48 -0400

    homework1 completed

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -38,7 +38,7 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
         if (isFull()) {
-            throw new IllegalArgumentException("Ring Buffer Overflow");
+            throw new RuntimeException("Ring Buffer Overflow");
         }
         rb[last] = x;
         last = (last + 1) % capacity;
@@ -54,7 +54,7 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
         if (isEmpty()) {
-            throw new IllegalArgumentException("Ring Buffer Underflow");
+            throw new RuntimeException("Ring Buffer Underflow");
         }
         T res = rb[first];
         first = (first + 1) % capacity;
@@ -68,6 +68,9 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     @Override
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
         return rb[first];
     }
 
